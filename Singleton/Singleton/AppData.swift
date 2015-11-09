6076d56b903c68f06363ea04bcafd9176ca1a769
Jageen shukla <Jageenshukla@yahoo.in>
2015-11-09 23:57:54 +0530

    Final code

--- Singleton/Singleton/AppData.swift
+++ Singleton/Singleton/AppData.swift
@@ -10,3 +10,43 @@
 AppData responsible for storing app data
 **/
 import Foundation
+
+class AppData
+{
+    // Create static variable so that for all object share
+    // same memory for this variable
+    static var instance:AppData!
+    
+    /**
+     For Demo purpose:
+     Store object created time
+     **/
+    var createdTime:NSDate!
+    
+    /**
+     For Demo purpose:
+     Store time for last request for create
+     object
+     **/
+    var lastReqObject:NSDate!
+    
+    /**
+     function which return shared instance of class
+     it will first check that is instance is created if not then
+     create and then return else return withought allocating new
+     memory
+     **/
+    class func sharedInstance()->AppData {
+        if(self.instance == nil)
+        {
+            self.instance = AppData()
+            
+            // Set object created time
+            self.instance.createdTime = NSDate()
+        }
+        
+        // Set time for last request for create
+        self.instance.lastReqObject = NSDate()
+        return self.instance;
+    }
+}
--- Singleton/Singleton/ViewController.swift
+++ Singleton/Singleton/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    /**
+     For Demo purpose:
+     Contain list of object of class AppData
+     **/
+    var objectArray:NSMutableArray!
+    
+    /**
+     Table view used to display list of AppData classes
+     **/
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Allocate memory to object array
+        self.objectArray = NSMutableArray()
+        
+        self.tblView.delegate = self
+        self.tblView.dataSource = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return objectArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = self.tblView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        let tmpAppData:AppData
+        tmpAppData = objectArray[indexPath.row] as! AppData
+        
+        // --- Little code for prepare cell for demo
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .FullStyle
+        dateFormatter.dateFormat = "HH:mm:ss"
+        
+        let objNumber = String(indexPath.row + 1)
+        cell.textLabel?.text = "Object number : " + objNumber
+        
+        cell.detailTextLabel?.text = "Object created time " + dateFormatter.stringFromDate(tmpAppData.createdTime) + "\nObject used time " + dateFormatter.stringFromDate(tmpAppData.lastReqObject)
+        // -----------
+        
+        return cell
+    }
 
+    /**
+     Create instance of AppData class then add it into objectArray
+     and reload table view
+     **/
+    @IBAction func btnCreateClicked(sender: AnyObject) {
+        let newObj = AppData.sharedInstance();
+        objectArray.addObject(newObj);
+        self.tblView.reloadData();
+    }
 }
 
