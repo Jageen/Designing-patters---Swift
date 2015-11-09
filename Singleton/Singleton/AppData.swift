692f869e6a055b4e59b0f46b67535e0ef78dae40
Jageen shukla <Jageenshukla@yahoo.in>
2015-11-09 19:58:57 +0530

    Create base

---
+++ Singleton/Singleton/AppData.swift
@@ -0,0 +1,12 @@
+//
+//  AppData.swift
+//  Singleton
+//
+//  Created by Jageen shukla on 09/11/15.
+//  Copyright © 2015 Red. All rights reserved.
+//
+
+/**
+AppData responsible for storing app data
+**/
+import Foundation
---
+++ Singleton/Singleton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Singleton
+//
+//  Created by Jageen shukla on 09/11/15.
+//  Copyright © 2015 Red. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
